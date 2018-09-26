fefb8e1a3fe12096db3a214a468fe13c51ab8ea5
immrhiep <daohuyhiep2810@gmail.com>
2018-09-26 20:31:45 +0700

    Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 __author__ = 'hiepdh2'
-
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Word"
+    user = dict(username='Mr Hiep')
+    posts = [dict(author=dict(username='Mr Hiep'), body='Beautiful day'),
+             dict(author=dict(username='Mr Nam'), body='Bad day')]
+    return render_template('index.html', title='Home page', user=user, posts=posts)
